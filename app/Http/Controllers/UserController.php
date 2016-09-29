a664e43d7c181368ecc33314839411f891fbfe1a
Joseph Taylor <josephtaylor.mn@gmail.com>
2016-09-28 20:37:03 -0500

    Friend Request email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,5 +11,15 @@ class UserController extends Controller {
 		$friendRequest->user_id = Auth::id();
 		$friendRequest->friend_id = $friendid;
 		$friendRequest->save();
+
+		$url = url('/user/acceptfriend/' . Auth::id());
+
+		$friend = App\User::find($friendid);
+
+		return (new MailMessage)
+			->greeting('Hello ' . $friend->name . '!')
+			->line(Auth::user()->name . ' has requested to be your friend and pray with you.')
+			->action('Accept Friendship', $url)
+			->line('Have a blessed day!');
 	}
 }
