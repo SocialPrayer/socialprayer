fbb5b6d4537483f462e52b131a6e6216b67936ae
Joseph Taylor <josephtaylor.mn@gmail.com>
2016-09-28 20:51:10 -0500

    Friend Request email

---
+++ app/Notifications/FriendRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class FriendRequest extends Notification {
+	use Queueable;
+
+	/**
+	 * Create a new notification instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($friendid) {
+		$this->friendid = $friendid;
+	}
+
+	/**
+	 * Get the notification's delivery channels.
+	 *
+	 * @param  mixed  $notifiable
+	 * @return array
+	 */
+	public function via($notifiable) {
+		return ['mail'];
+	}
+
+	/**
+	 * Get the mail representation of the notification.
+	 *
+	 * @param  mixed  $notifiable
+	 * @return \Illuminate\Notifications\Messages\MailMessage
+	 */
+	public function toMail($notifiable) {
+		$url = url('/user/acceptfriend/' . Auth::id());
+
+		$friend = \App\User::find($$this->friendid);
+
+		return (new MailMessage)
+			->subject('SocialPrayer - New Friendship Request')
+			->greeting('Hello ' . $friend->name . '!')
+			->line(Auth::user()->name . ' has requested to be your friend and pray with you.')
+			->action('Accept Friendship', $url)
+			->line('Have a blessed day!');
+	}
+
+	/**
+	 * Get the array representation of the notification.
+	 *
+	 * @param  mixed  $notifiable
+	 * @return array
+	 */
+	public function toArray($notifiable) {
+		return [
+			//
+		];
+	}
+}
