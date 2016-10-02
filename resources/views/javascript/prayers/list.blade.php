47ec92ca71f2baa428f630fc4e7896c84cf35d4e
josephmtaylor <joseph.taylor@dodatllc.com>
2016-10-02 10:37:33 -0500

    Ajax New Prayer Submit

--- resources/views/javascript/prayers/list.blade.php
+++ resources/views/javascript/prayers/list.blade.php
@@ -40,12 +40,54 @@ $(function(){
             //$('#success').html(response);
             //$('#success').hide('slow');
             var newPrayerDiv = $( ".prayer" ).first().clone();
+            newPrayerDiv.removeClass('panel-info');
+            newPrayerDiv.removeClass('panel-default');
+            newPrayerDiv.addClass('panel-success');
+            newPrayerDiv.find('.prayalong').data("id", 1);
+            newPrayerDiv.find('.prayalong').removeClass("btn-info");
+            newPrayerDiv.find('.prayalong').addClass("btn-default");
+            newPrayerDiv.find('.prayalong').removeClass("disabled");
+            newPrayerDiv.find('.prayer-user').html('<span data-toggle="popover" data-html="true" title="<b>{{ Auth::user()->name }}</b>" data-placement="top" data-content="" style="cursor: pointer;">{{ Auth::user()->name }}</span>');
             newPrayerDiv.find('.prayer-time').html('Now');
             newPrayerDiv.find('.prayer-text').html(prayerText);
             newPrayerDiv.prependTo(".prayers").hide().fadeIn('slow').slideDown('slow');
             $('#newPrayer')[0].reset();
             $('#newPrayerSubmit').prop('disabled','');
             $('.new-prayer-submit-text').html('Submit Prayer');
+
+            $('[data-toggle="popover"]').popover({ trigger: "manual" , html: true, animation:false})
+                .on("click", function () {
+                    var _this = this;
+                    $(this).popover("show");
+                    $(".popover").on("click", function () {
+                        $(_this).popover('hide');
+                    });
+                    $(".popover").on("mouseleave", function () {
+                        $(_this).popover('hide');
+                    });
+                    $(".container").on("mouseleave", function () {
+                        $(_this).popover('hide');
+                    });
+                }).on("mouseleave", function () {
+                    var _this = this;
+                    setTimeout(function () {
+                        if (!$(".popover:hover").length) {
+                            $(_this).popover("hide");
+                        }
+                    }, 300);
+            });
+            $('[data-toggle="tooltip"]').tooltip();
+
+            $('.prayalong').click(function(){
+                var prayer_id = $(this).data("id");
+                var $thisselector = $(this);
+                $(this).addClass("disabled");
+                $(this).removeClass("btn-default");
+                $(this).addClass("btn-info");
+                $.get('/prayer/pray-along/' + prayer_id, function( data ) {
+                    $thisselector.prev('.prayedalongcount').html(data);
+                });
+            });
         });
 
         // $.ajax({
