5d12ae5127d0cca8fb6378f1bc793fed76697985
Joseph Taylor <josephtaylor.mn@gmail.com>
2016-10-03 20:50:12 -0500

    Invite Friends

--- resources/views/users/friends/invite.blade.php
+++ resources/views/users/friends/invite.blade.php
@@ -9,7 +9,7 @@
   </div>
   <div class="modal-footer">
     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-primary">Send Invites</button>
+    <button type="submit" class="btn btn-primary">Send Invites</button>
   </div>
 </form>
 </div>
