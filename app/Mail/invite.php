7cacbe57b1c84736f36de727fd297eb80fe42471
josephmtaylor <joseph.taylor@dodatllc.com>
2016-10-04 19:45:49 -0500

    Invite Friends

--- app/Mail/invite.php
+++ app/Mail/invite.php
@@ -32,7 +32,7 @@ class invite extends Mailable {
 				'level' => 'info',
 				'subject' => 'SocialPrayer - ' . Auth::user()->name . ' just invited you to pray with them',
 				'greeting' => 'Greetings,',
-				'introLines' => ['You have been invited by ' . Auth::user()->name . ' to pray on SocialPrayer. A new place on the internet to pray either by yourself or together with those you know and love. Or even anonymously!'],
+				'introLines' => ['You have been invited by ' . Auth::user()->name . ' to pray on SocialPrayer. A new place on the internet to pray either by yourself or together with those you know and love. You can also pray anonymously and it will not save your name, or just with God and he will be the only one to see it. Ever!'],
 				'actionText' => 'Sign Up and Pray',
 				'actionUrl' => $url,
 				'outroLines' => ['Have a blessed day!'],
