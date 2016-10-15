6d52bfbc60fb418a2f37218661e4dacad7e5ae3a
Joseph Taylor <josephtaylor.mn@gmail.com>
2016-10-15 10:13:43 -0500

    Register to Join now

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -138,7 +138,7 @@
                 </div>
                 <div class="links">
                     <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
+                    <a href="{{ url('/register') }}">Join Now</a>
                 </div>
 
             </div>
