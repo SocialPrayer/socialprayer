3e8531980e7633095e2b148225fce7e27120f074
josephmtaylor <joseph.taylor@dodatllc.com>
2016-10-16 17:39:09 -0500

    Add middlename field

---
+++ database/migrations/2016_10_16_163549_alter_user_profile_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUserProfileTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('user_profiles', function ($table) {
+			$table->string('middlename')->nullable()->after('firstname');
+			$table->char('sex', 1)->nullable()->change();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('user_profiles', function ($table) {
+			$table->dropColumn('middlename');
+			$table->char('sex', 1)->change();
+		});
+	}
+}
