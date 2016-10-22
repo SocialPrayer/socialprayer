695ed57eedf944879b62326295495f501631e51a
Joseph Taylor <josephtaylor.mn@gmail.com>
2016-10-22 17:31:11 -0500

    Cleanup

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,11 +21,3 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
-
-$factory->define(App\Prayer::class, function (Faker\Generator $faker) {
-    return [
-        'text' => $faker->paragraph,
-        'user_id' => '1',
-        'privacy_setting_id' => '3'
-    ];
-});
