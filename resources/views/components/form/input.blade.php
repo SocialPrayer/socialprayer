922fab3dfc198a5fb9a0c040d5efccca236e2fb2
josephmtaylor <joseph.taylor@dodatllc.com>
2016-10-29 16:06:55 -0500

    Profile fixed

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -3,9 +3,9 @@
 @else
 	<?php $hidediv = "";?>
 @endif
-<div class="form-group"{{$hidediv}}>
-    	{{ Form::label($name, $label, ['class' => 'col-xs-3 control-label'], $attributes) }}
-    <div class="col-sm-8">
+<div class="form-group col-xs-12"{{$hidediv}}>
+    	{{ Form::label($name, $label, ['class' => 'col-xs-5 col-sm-3 text-right control-label'], $attributes) }}
+    <div class="col-xs-7 col-sm-9">
     	@if($type=='select')
     		{{ Form::$type($name, $value, $default, array_merge(['class' => 'form-control'], $attributes)) }}
     	@else
--- resources/views/users/profile/edit.blade.php
+++ resources/views/users/profile/edit.blade.php
@@ -1,78 +1,118 @@
-<div class="modal-header">
-	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	<h4 class="modal-title">Edit Profile</h4>
+<div class="modal-header" style="border-bottom: 0;">
+	<button data-dismiss="modal" class="close" aria-label="Close" class="btn btn-default">Close &times;</button>
+	<h3 class="modal-title text-center">Your Profile</h3>
+	<ul class="nav nav-tabs" role="tablist">
+	    <li role="presentation" class="active">
+	    	<a href="#home" aria-controls="home" role="tab" data-toggle="tab">
+	    		Personal Info
+	    	</a>
+	    </li>
+	    <!-- <li role="presentation">
+	    	<a href="#friends" aria-controls="friends" role="tab" data-toggle="tab">
+	    		Friends
+	    	</a>
+	    </li>
+	    <li role="presentation">
+	    	<a href="#alerts" aria-controls="alerts" role="tab" data-toggle="tab">
+	    		Alerts
+	    	</a>
+	    </li>
+	    <li role="presentation">
+	    	<a href="#picture" aria-controls="picture" role="tab" data-toggle="tab">
+	    		Profile Picture
+	    	</a>
+	    </li>
+	    <li role="presentation">
+	    	<a href="#stats" aria-controls="stats" role="tab" data-toggle="tab">
+	    		Stats
+	    	</a>
+	    </li> -->
+	</ul>
 </div>
-{{ Form::model($userProfile, ['route' => ['profile.update', $userProfile->id], 'id' => 'profileEdit']) }}
-<div class="modal-body form-horizontal">
-
-	<div class="form-group">
-		{{ csrf_field() }}
-		{{ Form::bsInput(
-			'email',
-			'email',
-			'Email Address:'
-		) }}
-		{{ Form::bsInput(
-			'text',
-			'firstname',
-			'First Name:'
-		) }}
-		{{ Form::bsInput(
-			'text',
-			'lastname',
-			'Last Name:'
-		) }}
-		{{ Form::bsInput(
-			'select',
-			'sex',
-			'Sex:',
-			[
-				'M' => 'Male',
-				'F' => 'Female'
-			],
-			[
-				'placeholder' => 'What is your gender?...'
-			]
-		) }}
-		{{ Form::bsInput(
-			'select',
-			'marital_status',
-			'Marital Status:',
-			[
-				'single' => 'Single',
-				'engaged' => 'Engaged',
-				'married' => 'Married',
-				'divorced' => 'Divorced'
-			],
-			[
-				'placeholder' => 'What is your Marital Status?...'
-			]
-		) }}
-		@if($userProfile->marital_status!='married')
+<div class="tab-content">
+	<div role="tabpanel" class="tab-pane active form-horizontal" id="home">
+		{{ Form::model($userProfile, ['route' => ['profile.update', $userProfile->id], 'id' => 'profileEdit']) }}
+		<div class="form-group">
+			{{ csrf_field() }}
+			{{ Form::bsInput(
+				'email',
+				'email',
+				'Email Address:'
+			) }}
 			{{ Form::bsInput(
 				'text',
-				'spouse_name',
-				'Spouse Name:',
-				null,
+				'firstname',
+				'First Name:'
+			) }}
+			{{ Form::bsInput(
+				'text',
+				'lastname',
+				'Last Name:'
+			) }}
+			{{ Form::bsInput(
+				'select',
+				'sex',
+				'Sex:',
 				[
-					'display-hide' => true,
+					'M' => 'Male',
+					'F' => 'Female'
+				],
+				[
+					'placeholder' => 'What is your gender?...'
 				]
 			) }}
-		@else
 			{{ Form::bsInput(
-				'text',
-				'spouse_name',
-				'Spouse Name:',
-				null
+				'select',
+				'marital_status',
+				'Marital Status:',
+				[
+					'single' => 'Single',
+					'engaged' => 'Engaged',
+					'married' => 'Married',
+					'divorced' => 'Divorced'
+				],
+				[
+					'placeholder' => 'What is your Marital Status?...'
+				]
 			) }}
-		@endif
-		<div class="col-xs-8 col-xs-offset-3">
-			<button type="submit" class="btn btn-primary">Submit</button>
-		</div>
-	</div>
+			@if($userProfile->marital_status!='married')
+				{{ Form::bsInput(
+					'text',
+					'spouse_name',
+					'Spouse Name:',
+					null,
+					[
+						'display-hide' => true,
+					]
+				) }}
+			@else
+				{{ Form::bsInput(
+					'text',
+					'spouse_name',
+					'Spouse Name:',
+					null
+				) }}
+			@endif
+			<div class="text-center">
 
+				<button type="submit" class="btn btn-lg btn-primary">Submit</button>
+			</div>
+		</div>
+		{{ Form::close() }}
+	</div>
+	<div role="tabpanel" class="tab-pane" id="friends">
+		<!--Friends go here -->
+	</div>
+	<div role="tabpanel" class="tab-pane" id="alerts">
+		<!--Friends go here -->
+	</div>
+	<div role="tabpanel" class="tab-pane" id="picture">
+		<!--Friends go here -->
+	</div>
+	<div role="tabpanel" class="tab-pane" id="stats">
+		<!--Friends go here -->
+	</div>
 </div>
-{{ Form::close() }}
  <script>
 $(function(){
 	$('#marital_status').on('change',function(e) {
