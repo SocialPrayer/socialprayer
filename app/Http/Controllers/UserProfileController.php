1dbd055620be92636e005fdc84fa5f80304e73f1
Joseph Taylor <josephtaylor.mn@gmail.com>
2016-10-30 16:42:22 -0500

    fix

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -66,7 +66,7 @@ class UserProfileController extends Controller {
 	 */
 	public function update(Request $request, $id) {
 
-		$userProfile = UserProfile::find($ides);
+		$userProfile = UserProfile::find($id);
 		$userProfile->email = $request->email;
 		$userProfile->firstname = $request->firstname;
 		$userProfile->lastname = $request->lastname;
