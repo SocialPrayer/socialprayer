f64b22125e448f812869cc842694db3eae3c8b64
Joseph Taylor <josephtaylor.mn@gmail.com>
2016-12-01 12:07:37 -0600

    Fix invite email

--- app/Mail/invite.php
+++ app/Mail/invite.php
@@ -27,14 +27,22 @@ class invite extends Mailable {
 	 * @return $this
 	 */
 	public function build() {
+	
 		$url = env('APP_URL') . "/register";
+		
+		$personalMessage = "";
+		
+		if($this->message != ""){ 
+				$personalMessage = '<br />Personal Message from ' . Auth::user()->name . ': ' . $message;
+		}
+		
 		return $this->view('vendor.notifications.email')
 			->subject('SocialPrayer - ' . Auth::user()->name . ' just invited you to pray with them')
 			->with([
 				'level' => 'info',
 				'subject' => 'SocialPrayer - ' . Auth::user()->name . ' just invited you to pray with them',
 				'greeting' => 'Greetings,',
-				'introLines' => ['You have been invited by ' . Auth::user()->name . ' to pray on SocialPrayer. A new place on the internet to pray either by yourself or together with those you know and love. You can also pray anonymously and it will not save your name, or just with God and he will be the only one to see it. Ever!' . if($this->message != ""){ . '<br />Personal Message from ' . Auth::user()->name . ': ' . $message}],
+				'introLines' => ['You have been invited by ' . Auth::user()->name . ' to pray on SocialPrayer. A new place on the internet to pray either by yourself or together with those you know and love. You can also pray anonymously and it will not save your name, or just with God and he will be the only one to see it. Ever!' . $personalMessage],
 				'actionText' => 'Sign Up and Pray',
 				'actionUrl' => $url,
 				'outroLines' => ['Have a blessed day!'],
